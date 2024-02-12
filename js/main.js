088f817946387618d60a26e1658313db3541c3f8
Javier Medina <javi86medina@hotmail.com>
2024-02-12 20:56:07 +0100

    stylos hasta about

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 // toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+});
+
+/*menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};*/
 
 // scroll sections
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
+
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
@@ -11,17 +23,28 @@ window.onscroll = () => {
         let height = sec.offsetHeight;
         let id= sec.getAttribute('id');
 
-if(top >= offset && top < offset + height)  {
-    // desliza el lik del menu  al que corresponda
-    navLinks.forEach(links => {
-        links.classList.remove('active');
-        document.querySelector('header nav a[href*='+ id +']').classList.add('active');
+    if(top >= offset && top < offset + height)  {
+    // active navbar links
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+        document.querySelector('header nav a[href*='+ id + ']').classList.add('active');
     });
+
+    // active sections for animation on scroll
+    sec.classList.add('show-animate');
+
+    // if want to use animation that repeats on scroll use this
+     
+    
 }
     });
 
     //sticky header
     let header = document.querySelector('header');
 
-    header.classList.toggle('sticky',window.screenY > 100);
+    header.classList.toggle('sticky',window.scrollY > 100);
+
+    // remove toggle icon and navbar when click navbar links (scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 }
