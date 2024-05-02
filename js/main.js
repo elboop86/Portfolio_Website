606258fd6a04bd1e47985b702d4e773fd5130d83
Javier Medina <javi86medina@hotmail.com>
2024-05-02 12:57:53 +0200

    Actualizado

--- js/main.js
+++ js/main.js
@@ -25,8 +25,8 @@ window.onscroll = () => {
 
     if(top >= offset && top < offset + height)  {
     // active navbar links
-    navLinks.forEach(link => {
-        link.classList.remove('active');
+    navLinks.forEach(links => {
+        links.classList.remove('active');
         document.querySelector('header nav a[href*='+ id + ']').classList.add('active');
     });
 
